6c268cc875df38e8eacc38d5aef5afe2cec708a6
adrianojedaf <adrianojedaf@hotmail.com>
2022-04-26 23:56:04 +0200

    update js files

---
+++ js/scroll-down.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  const stageScrollDownButton = $('.stage-button');
+  const header = $('nav');
+  const ueberUnsLink = $('a.ueber-uns');
+  const produkteLink = $('a.produkte');
+
+  [stageScrollDownButton, ueberUnsLink].forEach(element => {
+
+    element.on('click', function(e) {
+      e.preventDefault();
+  
+      $('html,body').animate({
+        scrollTop: $(window).height() - header.height()
+      }, 'slow');
+    });
+  })
+
+
+  produkteLink.on('click', function(e) {
+    e.preventDefault();
+    
+    $('html,body').animate({
+      scrollTop: $(window).height() - header.height() + 800
+    }, 'slow');
+  });
+                                              
+});
