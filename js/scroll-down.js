c42c6c11ee314ec7117fc9beb0cc8f0062ddc79c
adrianojedaf <adrianojedaf@hotmail.com>
2022-05-03 22:03:23 +0200

    delete empty lines, add lines where required and other fixes

--- js/scroll-down.js
+++ js/scroll-down.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
     });
   })
 
-
   produkteLink.on('click', function(e) {
     e.preventDefault();
     
--- js/side-widget.js
+++ js/side-widget.js
@@ -1,9 +1,7 @@
 const sideWidget = document.querySelector('.social-media-bar');
 const stage = document.querySelector('.stage');
 const isDesktop = window.matchMedia("(min-width: 1024px)");
-
 const stageHeight = stage.offsetHeight;
-
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > stageHeight) {
