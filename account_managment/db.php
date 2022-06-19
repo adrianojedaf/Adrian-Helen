796bfada896c344463f3e61eee89b1cf64bce901
adrianojedaf <adrianojedaf@hotmail.com>
2022-06-19 23:01:39 +0200

    delete unecessary coments in db

--- account_managment/db.php
+++ account_managment/db.php
@@ -4,16 +4,6 @@
     $db_password = '';
     $db_name = 'xcooter';
 
-    //create conenction
-    // $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
-
-    // Check connection
-    // if (mysqli_connect_errno()) {
-    //     echo "Failed to connect!";
-    //     exit();
-    // }
-
-
     try {
         $conn = new PDO("mysql:host=$db_server;dbname=$db_name;", $db_username, $db_password);
     } catch (PDOException $e) {
